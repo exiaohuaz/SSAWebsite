403484595551bc9636d3258dea02384782b1603e
exiaohuaz <64718742+exiaohuaz@users.noreply.github.com>
2021-01-25 14:36:19 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+var slideIndex = 0;
+showSlides();
+
+var drop = document.getElementsByClassName("dropdown");
+var i;
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+
+for (i = 0; i < dropdown.length; i++) {
+  drop[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
